7f572ba840f7dacd8596f5d4bdb5f7aa580531fe
Jan Niklas Schreder <90890863+JaSc2912@users.noreply.github.com>
2023-07-26 14:53:49 +0200

    Further solutions, last one pending

--- Final.py
+++ Final.py
@@ -1,11 +1,20 @@
-# Aufgabe: Importiere die benötigten Bibliotheken pandas und matplotlib​
+#Importiere die benötigten Bibliotheken pandas und matplotlib​
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from google.colab import files
+#Öffne einen Dateidialog, um eine CSV-Datei hochzuladen​
+uploaded_files = files.upload()
+#Lies die hochgeladene CSV-Datei mit Pandas ein​
+pd.read_csv(next(iter(uploaded_files)))
+#Gib die ersten Zeilen des DataFrames aus, um sicherzustellen, dass die Daten richtig eingelesen wurden​
+print(df.head())
+#Plotte die Daten aus dem DataFrame mit Matplotlib​
 
-# Aufgabe: Öffne einen Dateidialog, um eine CSV-Datei hochzuladen​
-
-# Aufgabe: Lies die hochgeladene CSV-Datei mit Pandas ein​
-
-# Aufgabe: Lies die hochgeladene CSV-Datei mit Pandas ein​
-
-# Aufgabe: Gib die ersten Zeilen des DataFrames aus, um sicherzustellen, dass die Daten richtig eingelesen wurden​
-
-# Aufgabe: Plotte die Daten aus dem DataFrame mit Matplotlib​
+# plt.figure(figsize=(10, 6))  # Größe des Plots festlegen
+plt.plot(df.index, df['Werte'], marker='o', linestyle='-')  # Daten plotten mit Index als x-Achse und 'Werte' als y-Achse
+# plt.xlabel('Index')  # Beschriftung der x-Achse
+# plt.ylabel('Werte')  # Beschriftung der y-Achse
+# plt.title('CSV-Datenplot')  # Titel des Plots
+# plt.grid(True)  # Gitter anzeigen
+plt.show()  # Plot anzeigen
--- UploadAndPrintCSV.py
+++ UploadAndPrintCSV.py
@@ -1,5 +1,11 @@
 #Importiere die auf der Folie genannte Bibliothek
-From google.collab import files
+from google.colab import files
+
 #Öffne einen Dateidialog und speichere die Datei in einer Variablen
+
+uploaded_files = files.upload()
+
+for filename, file_content in uploaded_files.items():
+ print(file_content)
 
 #Gebe jede Zeile auf der Konsole aus Tipp: vgl. vorherige Aufgaben
